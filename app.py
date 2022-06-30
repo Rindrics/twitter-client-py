0eced917c067dc955cbd7daa243e57e26564a7b9
Akira Hayashi <smxshxishxad@me.com>
2022-06-30 23:43:53 +0900

    :sparkles: Serve timeline on root

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from tweet import home_timeline
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    text = home_timeline()
+    return f"""
+<h1>Hello from Flask-Docker!</h1>
+
+{"".join(text)}
+"""
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
